de52fdae9522fc905a8b5bf710889ea7d927e858
Tyler Zhang <t-veor@users.noreply.github.com>
2020-03-16 00:27:27 +0000

    Add WIP Todd-Coxeter algorithm for generating regular polychora

--- src/mesh/mod.rs
+++ src/mesh/mod.rs
@@ -1,5 +1,12 @@
-use cgmath::Vector4;
+mod todd_coxeter;
+
+use cgmath::{InnerSpace, Vector4, Zero};
 use smallvec::SmallVec;
+
+#[derive(Debug, Clone)]
+pub struct VertexData {
+    cells: SmallVec<[usize; 16]>,
+}
 
 #[derive(Debug, Clone)]
 pub struct Edge {
@@ -18,14 +25,166 @@ pub struct Face {
 #[derive(Debug, Clone)]
 pub struct Cell {
     normal: Vector4<f32>,
-    faces: SmallVec<[usize; 8]>,
+    faces: SmallVec<[usize; 16]>,
 }
 
 pub struct Mesh {
     vertices: Vec<Vector4<f32>>,
+    vertex_data: Vec<VertexData>,
     edges: Vec<Edge>,
     faces: Vec<Face>,
     cells: Vec<Cell>,
 }
 
-impl Mesh {}
+fn reflect(v: Vector4<f32>, mirror_normal: Vector4<f32>) -> Vector4<f32> {
+    v - 2.0 * mirror_normal.dot(v) * mirror_normal
+}
+
+fn get_mirror_normals(symbol: &[usize; 3]) -> [Vector4<f32>; 4] {
+    use std::f32::consts::PI;
+
+    let mut mirror_normals = [Vector4::zero(); 4];
+
+    mirror_normals[0] = Vector4::unit_x();
+
+    // dot(N_0, N_1) = cos(pi / symbol[0])
+    mirror_normals[1].x = (PI / symbol[0] as f32).cos();
+    mirror_normals[1].y = (1.0 - mirror_normals[1].x.powi(2)).sqrt();
+
+    // dot(N_0, N_2) = cos(pi / 2) = 0
+    // dot(N_1, N_2) = cos(pi / symbol[1])
+    mirror_normals[2].y = (PI / symbol[1] as f32).cos() / mirror_normals[1].y;
+    mirror_normals[2].z = (1.0 - mirror_normals[2].y.powi(2)).sqrt();
+
+    // dot(N_0, N_3) = cos(pi / 2) = 0
+    // dot(N_1, N_3) = cos(pi / 2) = 0
+    // dot(N_2, N_3) = cos(pi / symbol[2])
+    mirror_normals[3].z = (PI / symbol[2] as f32).cos() / mirror_normals[2].z;
+    mirror_normals[3].w = (1.0 - mirror_normals[3].z.powi(2)).sqrt();
+
+    mirror_normals
+}
+
+impl Mesh {
+    pub fn from_schlafli_symbol(symbol: &[usize; 3]) {
+        // determine the mirror normals
+        let mirror_normals = get_mirror_normals(symbol);
+
+        // pick a v0 so that it's on planes 1, 2, and 3, but not on 0
+        // set v_0.x = 1 arbitrarily
+        let mut v0: Vector4<f32> = Vector4::unit_x();
+        v0.y = -mirror_normals[1].x * v0.x / mirror_normals[1].y;
+        v0.z = -mirror_normals[2].y * v0.y / mirror_normals[2].z;
+        v0.w = -mirror_normals[3].z * v0.z / mirror_normals[3].w;
+        v0 = v0.normalize();
+
+        // setup for todd-coxeter
+        let num_gens = 4;
+        let relations: &[&[usize]] = &[
+            &[0, 0],
+            &[1, 1],
+            &[2, 2],
+            &[3, 3],
+            &[0, 1].repeat(symbol[0]),
+            &[1, 2].repeat(symbol[1]),
+            &[2, 3].repeat(symbol[2]),
+            &[0, 2].repeat(2),
+            &[0, 3].repeat(2),
+            &[1, 3].repeat(2),
+        ];
+
+        let vertex_table =
+            todd_coxeter::coset_table(num_gens, relations, &[1, 2, 3]);
+        let vertices: Vec<_> = todd_coxeter::coset_table_bfs(&vertex_table, 0)
+            .iter()
+            .map(|mirrors| {
+                mirrors
+                    .iter()
+                    .fold(v0, |v, i| reflect(v, mirror_normals[*i]))
+            })
+            .collect();
+
+        let edge_table =
+            todd_coxeter::coset_table(num_gens, relations, &[0, 2, 3]);
+        // the initial edge is guaranteed to be (0, 1)
+        let e0 = (0, 1);
+        let edge_tmp: Vec<_> = todd_coxeter::coset_table_bfs(&edge_table, 0)
+            .iter()
+            .map(|mirrors| {
+                mirrors.iter().fold(e0, |(v0, v1), i| {
+                    (vertex_table[v0][*i], vertex_table[v1][*i])
+                })
+            })
+            .collect();
+
+        let face_table =
+            todd_coxeter::coset_table(num_gens, relations, &[0, 1, 3]);
+        // the initial face is all the edges invariant under the rotation (0, 1)
+        let f0 = {
+            let mut edges: SmallVec<[usize; 8]> = SmallVec::new();
+            let mut curr_edge = 0;
+            loop {
+                edges.push(curr_edge);
+                curr_edge = edge_table[edge_table[curr_edge][0]][1];
+                if curr_edge == 0 {
+                    break;
+                }
+            }
+            edges
+        };
+        let face_tmp: Vec<_> = todd_coxeter::coset_table_bfs(&face_table, 0)
+            .iter()
+            .map(|mirrors| {
+                mirrors.iter().fold(f0.clone(), |f, i| {
+                    f.into_iter().map(|e| edge_table[e][*i]).collect()
+                })
+            })
+            .collect();
+
+        let cell_table =
+            todd_coxeter::coset_table(num_gens, relations, &[0, 1, 2]);
+        // The initial cell is invariant under mirrors 0, 1, and 2.
+        // So, if we just apply mirrors 0, 1, and 2 to the inital face a
+        // whole bunch of times, we should recover all the faces in the initial
+        // cell
+        let c0 = {
+            let mut faces: SmallVec<[usize; 16]> = SmallVec::new();
+            faces.push(0);
+            let mut i = 0;
+            while i < faces.len() {
+                let f = faces[i];
+                for j in 0..3 {
+                    let new_face = face_table[f][j];
+                    if !faces.contains(&new_face) {
+                        faces.push(new_face);
+                    }
+                }
+                i += 1;
+            }
+            faces
+        };
+        let cell_tmp: Vec<_> = todd_coxeter::coset_table_bfs(&cell_table, 0)
+            .iter()
+            .map(|mirrors| {
+                mirrors.iter().fold(c0.clone(), |c, i| {
+                    c.into_iter().map(|e| face_table[e][*i]).collect()
+                })
+            })
+            .collect();
+        dbg!(cell_tmp);
+
+        // dbg!(vertices);
+        // dbg!(edge_tmp);
+        // dbg!(face_tmp);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn schlafli() {
+        Mesh::from_schlafli_symbol(&[3, 3, 3]);
+    }
+}
---
+++ src/mesh/todd_coxeter.rs
@@ -0,0 +1,227 @@
+use std::collections::{HashMap, HashSet, VecDeque};
+
+struct CosetTable {
+    num_gens: usize,
+    table: Vec<Vec<Option<usize>>>,
+    live_map: Vec<usize>,
+}
+
+impl CosetTable {
+    fn init(num_gens: usize) -> Self {
+        let table = vec![vec![None; num_gens]];
+        let live_map = vec![0];
+
+        CosetTable {
+            num_gens,
+            table,
+            live_map,
+        }
+    }
+
+    fn define(&mut self, coset: usize, g: usize) {
+        let fresh = self.table.len();
+        self.table.push(vec![None; self.num_gens]);
+        self.table[coset][g] = Some(fresh);
+        self.table[fresh][g] = Some(coset);
+        self.live_map.push(fresh);
+    }
+
+    fn rep(&mut self, coset: usize) -> usize {
+        let mut m = coset;
+        while m != self.live_map[m] {
+            m = self.live_map[m];
+        }
+
+        let mut j = coset;
+        while j != self.live_map[j] {
+            let next = self.live_map[j];
+            self.live_map[j] = m;
+            j = next;
+        }
+
+        m
+    }
+
+    fn merge(&mut self, queue: &mut Vec<usize>, coset1: usize, coset2: usize) {
+        let (s, t) = (self.rep(coset1), self.rep(coset2));
+        if s != t {
+            let (s, t) = (s.min(t), s.max(t));
+            self.live_map[t] = s;
+            queue.push(t);
+        }
+    }
+
+    fn coincidence(&mut self, coset1: usize, coset2: usize) {
+        let mut queue = Vec::new();
+
+        self.merge(&mut queue, coset1, coset2);
+        while queue.len() != 0 {
+            let e = queue.remove(0);
+            for g in 0..self.num_gens {
+                if let Some(f) = self.table[e][g] {
+                    self.table[f][g] = None;
+
+                    let (e_, f_) = (self.rep(e), self.rep(f));
+                    if let Some(x) = self.table[e_][g] {
+                        self.merge(&mut queue, f_, x);
+                    } else if let Some(x) = self.table[f_][g] {
+                        self.merge(&mut queue, e_, x);
+                    } else {
+                        self.table[e_][g] = Some(f_);
+                        self.table[f_][g] = Some(e_);
+                    }
+                }
+            }
+        }
+    }
+
+    fn scan_and_fill(&mut self, coset: usize, word: &[usize]) {
+        let mut f = coset;
+        let mut b = coset;
+        let mut i = 0;
+        let mut j = word.len() - 1;
+
+        loop {
+            // scan forwards as far as possible
+            while i <= j && self.table[f][word[i]].is_some() {
+                f = self.table[f][word[i]].unwrap();
+                i += 1;
+            }
+
+            if i > j {
+                if f != b {
+                    // found a coincidence
+                    self.coincidence(f, b);
+                }
+                return;
+            }
+
+            while j >= i && self.table[b][word[j]].is_some() {
+                b = self.table[b][word[j]].unwrap();
+                j -= 1;
+            }
+
+            if j < i {
+                self.coincidence(f, b);
+                return;
+            } else if i == j {
+                // deduction
+                self.table[f][word[i]] = Some(b);
+                self.table[b][word[i]] = Some(f);
+            } else {
+                // define a new coset, continue scanning
+                self.define(f, word[i]);
+            }
+        }
+    }
+}
+
+pub fn coset_table(
+    num_gens: usize,
+    relations: &[&[usize]],
+    sub_gens: &[usize],
+) -> Vec<Vec<usize>> {
+    let mut table = CosetTable::init(num_gens);
+
+    // fill in initial information for the first coset
+    for g in sub_gens {
+        table.scan_and_fill(0, &[*g]);
+    }
+
+    let mut i = 0;
+    while i < table.table.len() {
+        if table.live_map[i] == i {
+            for rel in relations {
+                table.scan_and_fill(i, rel);
+            }
+
+            for g in 0..num_gens {
+                if table.table[i][g].is_none() {
+                    table.define(i, g);
+                }
+            }
+        }
+
+        i += 1;
+    }
+
+    // compress the resulting table
+    let mut forward_map = HashMap::new();
+    let mut backward_map = HashMap::new();
+    let mut fresh = 0;
+    for coset in 0..table.table.len() {
+        if table.live_map[coset] == coset {
+            forward_map.insert(coset, fresh);
+            backward_map.insert(fresh, coset);
+            fresh += 1;
+        }
+    }
+
+    let mut compressed_table = Vec::new();
+    for i in 0..fresh {
+        compressed_table.push(
+            table.table[backward_map[&i]]
+                .iter()
+                .map(|x| forward_map[&x.unwrap()])
+                .collect(),
+        );
+    }
+
+    compressed_table
+}
+
+pub fn coset_table_bfs(
+    table: &Vec<Vec<usize>>,
+    start: usize,
+) -> Vec<Vec<usize>> {
+    let mut paths = vec![Vec::new(); table.len()];
+    let mut queue = VecDeque::new();
+    let mut seen = HashSet::new();
+
+    queue.push_back(start);
+    seen.insert(start);
+
+    while let Some(top) = queue.pop_front() {
+        for (g, next) in table[top].iter().enumerate() {
+            if seen.contains(&next) {
+                continue;
+            }
+
+            paths[*next] = paths[top].clone();
+            paths[*next].push(g);
+            queue.push_back(*next);
+            seen.insert(*next);
+        }
+    }
+
+    paths
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn dodecahedron_test() {
+        let table = coset_table(
+            4,
+            &[
+                &[0, 0],
+                &[1, 1],
+                &[2, 2],
+                &[3, 3],
+                &[0, 1].repeat(5),
+                &[1, 2].repeat(3),
+                &[2, 3].repeat(3),
+                &[0, 2].repeat(2),
+                &[0, 3].repeat(2),
+                &[1, 3].repeat(2),
+            ],
+            &[0, 1, 3],
+        );
+        println!("{:?}", table);
+
+        let directions = coset_table_bfs(&table, 0);
+        println!("{:?}", directions);
+    }
+}
