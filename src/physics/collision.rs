10f66ea10c12aa3638435ccbdefa68165e290d5b
Tyler Zhang <t-veor@users.noreply.github.com>
2020-03-29 17:02:18 +0100

    Solve all constraints at once

--- src/physics/collision.rs
+++ src/physics/collision.rs
@@ -1,28 +1,167 @@
-use super::{Body, CollisionManifold, Velocity};
+use super::{Body, CollisionManifold};
 use crate::alg::Vec4;
-use cgmath::{Array, InnerSpace, Vector3, Vector4, Zero};
-use rand::seq::SliceRandom;
-
-pub const SOLVE_ITERS: usize = 20;
-
-pub struct Impulse {
-    pub impulse: Vector4<f32>,
-    pub position: Vector4<f32>,
-}
-
-pub struct CollisionResponse {
-    pub impulses: Vec<Impulse>,
-    pub projection: Vector4<f32>,
-}
+use cgmath::{InnerSpace, Vector3, Vector4};
 
 #[derive(Debug)]
-struct ContactState {
+pub struct ContactState {
     contact: Vector4<f32>,
     bias: f32,
     normal_mass: f32,
     normal_impulse: f32,
     tangent_mass: [f32; 3],
     tangent_impulse: [f32; 3],
+}
+
+pub struct CollisionConstraint {
+    normal: Vector4<f32>,
+    tangents: [Vector4<f32>; 3],
+    contacts: Vec<ContactState>,
+    e: f32,
+    mu: f32,
+}
+
+impl CollisionConstraint {
+    pub fn new(
+        manifold: CollisionManifold,
+        a: &Body,
+        mass_adjustment_a: f32,
+        b: &Body,
+        mass_adjustment_b: f32,
+    ) -> Self {
+        let CollisionManifold {
+            normal,
+            depth,
+            contacts,
+        } = manifold;
+
+        let e = a.material.restitution.min(b.material.restitution);
+        // TODO: move this into the Material struct
+        let mu = 1.5;
+
+        let tangents = compute_basis(normal);
+
+        let mut contacts: Vec<_> = contacts
+            .into_iter()
+            .map(|contact| {
+                let rel_vel = b.vel_at(contact) - a.vel_at(contact);
+                let rel_vel_normal = rel_vel.dot(normal);
+
+                let bias = if rel_vel_normal < -1.0 {
+                    -e * rel_vel_normal
+                } else {
+                    0.0
+                };
+
+                let inv_a_mass = if a.mass > 0.0 {
+                    mass_adjustment_a / a.mass
+                } else {
+                    0.0
+                };
+                let inv_b_mass = if b.mass > 0.0 {
+                    mass_adjustment_b / b.mass
+                } else {
+                    0.0
+                };
+
+                let inverse_mass_term =
+                    |body: &Body,
+                     normal: Vector4<f32>,
+                     contact: Vector4<f32>| {
+                        // n' = ~R n R
+                        let body_normal = body.world_vec_to_body(normal);
+                        let body_contact = body.world_pos_to_body(contact);
+
+                        // n . (R x . I_b^-1(x /\ n') ~R)
+                        normal.dot(
+                            body.body_vec_to_world(
+                                Vec4::from(body_contact)
+                                    .left_contract_bv(
+                                        &body.inverse_moment_of_inertia(
+                                            &Vec4::from(body_contact)
+                                                .wedge_v(&body_normal.into()),
+                                        ),
+                                    )
+                                    .into(),
+                            ),
+                        )
+                    };
+
+                let inv_l_a =
+                    mass_adjustment_a * inverse_mass_term(a, normal, contact);
+                let inv_l_b =
+                    mass_adjustment_b * inverse_mass_term(b, normal, contact);
+
+                let normal_mass =
+                    1.0 / (inv_a_mass + inv_b_mass + inv_l_a + inv_l_b);
+
+                let mut tangent_mass = [0.0, 0.0, 0.0];
+                for i in 0..3 {
+                    let inv_l_t_a = mass_adjustment_a
+                        * inverse_mass_term(a, tangents[i], contact);
+                    let inv_l_t_b = mass_adjustment_b
+                        * inverse_mass_term(b, tangents[i], contact);
+
+                    tangent_mass[i] =
+                        1.0 / (inv_a_mass + inv_b_mass + inv_l_t_a + inv_l_t_b);
+                }
+
+                ContactState {
+                    contact,
+                    bias,
+                    normal_mass,
+                    normal_impulse: 0.0,
+                    tangent_mass,
+                    tangent_impulse: [0.0, 0.0, 0.0],
+                }
+            })
+            .collect();
+
+        Self {
+            normal,
+            tangents,
+            contacts,
+            e,
+            mu,
+        }
+    }
+
+    pub fn solve(&mut self, a: &mut Body, b: &mut Body) {
+        for contact_state in self.contacts.iter_mut() {
+            let ContactState {
+                contact,
+                bias,
+                normal_mass,
+                ref mut normal_impulse,
+                tangent_mass,
+                ref mut tangent_impulse,
+            } = *contact_state;
+
+            let rel_vel = b.vel_at(contact) - a.vel_at(contact);
+
+            // calculate friction impulse
+            for i in 0..3 {
+                let lambda = -rel_vel.dot(self.tangents[i]) * tangent_mass[i];
+                let max_lambda = self.mu * *normal_impulse;
+                let prev_impulse = tangent_impulse[i];
+                tangent_impulse[i] =
+                    (prev_impulse + lambda).min(max_lambda).max(-max_lambda);
+                let impulse =
+                    self.tangents[i] * (tangent_impulse[i] - prev_impulse);
+                a.resolve_impulse(-impulse, contact);
+                b.resolve_impulse(impulse, contact);
+            }
+
+            // calculate normal impulse
+            let rel_vel = b.vel_at(contact) - a.vel_at(contact);
+            let rel_vel_normal = rel_vel.dot(self.normal);
+            let lambda = normal_mass * (-rel_vel_normal + bias);
+            let prev_impulse = *normal_impulse;
+            *normal_impulse = (prev_impulse + lambda).max(0.0);
+            let impulse = self.normal * (*normal_impulse - prev_impulse);
+            a.resolve_impulse(-impulse, contact);
+            b.resolve_impulse(impulse, contact);
+        }
+    }
 }
 
 // Extension of https://box2d.org/posts/2014/02/computing-a-basis/ to 4 dimensions.
@@ -47,149 +186,6 @@ fn compute_basis(a: Vector4<f32>) -> [Vector4<f32>; 3] {
     let d = crate::alg::triple_cross_product(a, b, c).normalize();
     [b, c, d]
 }
-
-pub fn calc_impulse(
-    instance: CollisionManifold,
-    a: &mut Body,
-    mass_adjustment_a: f32,
-    b: &mut Body,
-    mass_adjustment_b: f32,
-) -> CollisionResponse {
-    let CollisionManifold {
-        normal,
-        depth,
-        contacts,
-    } = instance;
-
-    let e = a.material.restitution.min(b.material.restitution);
-    // TODO: move this into the Material struct
-    let mu = 1.5;
-
-    let tangents = compute_basis(normal);
-
-    let mut contacts: Vec<_> = contacts
-        .into_iter()
-        .map(|contact| {
-            let rel_vel = b.vel_at(contact) - a.vel_at(contact);
-            let rel_vel_normal = rel_vel.dot(normal);
-
-            let bias = if rel_vel_normal < -1.0 {
-                -e * rel_vel_normal
-            } else {
-                0.0
-            };
-
-            let inv_a_mass = if a.mass > 0.0 {
-                mass_adjustment_a / a.mass
-            } else {
-                0.0
-            };
-            let inv_b_mass = if b.mass > 0.0 {
-                mass_adjustment_b / b.mass
-            } else {
-                0.0
-            };
-
-            let inverse_mass_term =
-                |body: &Body, normal: Vector4<f32>, contact: Vector4<f32>| {
-                    // n' = ~R n R
-                    let body_normal = body.world_vec_to_body(normal);
-                    let body_contact = body.world_pos_to_body(contact);
-
-                    // n . (R x . I_b^-1(x /\ n') ~R)
-                    normal.dot(
-                        body.body_vec_to_world(
-                            Vec4::from(body_contact)
-                                .left_contract_bv(
-                                    &body.inverse_moment_of_inertia(
-                                        &Vec4::from(body_contact)
-                                            .wedge_v(&body_normal.into()),
-                                    ),
-                                )
-                                .into(),
-                        ),
-                    )
-                };
-
-            let inv_l_a =
-                mass_adjustment_a * inverse_mass_term(a, normal, contact);
-            let inv_l_b =
-                mass_adjustment_b * inverse_mass_term(b, normal, contact);
-
-            let normal_mass =
-                1.0 / (inv_a_mass + inv_b_mass + inv_l_a + inv_l_b);
-
-            let mut tangent_mass = [0.0, 0.0, 0.0];
-            for i in 0..3 {
-                let inv_l_t_a = mass_adjustment_a
-                    * inverse_mass_term(a, tangents[i], contact);
-                let inv_l_t_b = mass_adjustment_b
-                    * inverse_mass_term(b, tangents[i], contact);
-
-                tangent_mass[i] =
-                    1.0 / (inv_a_mass + inv_b_mass + inv_l_t_a + inv_l_t_b);
-            }
-
-            ContactState {
-                contact,
-                bias,
-                normal_mass,
-                normal_impulse: 0.0,
-                tangent_mass,
-                tangent_impulse: [0.0, 0.0, 0.0],
-            }
-        })
-        .collect();
-
-    for _ in 0..SOLVE_ITERS {
-        for contact_state in contacts.iter_mut() {
-            let ContactState {
-                contact,
-                bias,
-                normal_mass,
-                ref mut normal_impulse,
-                tangent_mass,
-                ref mut tangent_impulse,
-            } = *contact_state;
-
-            let rel_vel = b.vel_at(contact) - a.vel_at(contact);
-
-            // calculate friction impulse
-            for i in 0..3 {
-                let lambda = -rel_vel.dot(tangents[i]) * tangent_mass[i];
-                let max_lambda = mu * *normal_impulse;
-                let prev_impulse = tangent_impulse[i];
-                tangent_impulse[i] =
-                    (prev_impulse + lambda).min(max_lambda).max(-max_lambda);
-                let impulse = tangents[i] * (tangent_impulse[i] - prev_impulse);
-                a.resolve_impulse(-impulse, contact);
-                b.resolve_impulse(impulse, contact);
-            }
-
-            // calculate normal impulse
-            let rel_vel = b.vel_at(contact) - a.vel_at(contact);
-            let rel_vel_normal = rel_vel.dot(normal);
-            let lambda = normal_mass * (-rel_vel_normal + bias);
-            let prev_impulse = *normal_impulse;
-            *normal_impulse = (prev_impulse + lambda).max(0.0);
-            let impulse = normal * (*normal_impulse - prev_impulse);
-            a.resolve_impulse(-impulse, contact);
-            b.resolve_impulse(impulse, contact);
-        }
-    }
-
-    // Do some linear projection to stop bodies from just sinking into
-    // each other
-    let slop_limit = 0.01f32;
-    let slop_amount = 0.8f32;
-    let projection = (depth - slop_limit).max(0.0) * slop_amount * normal;
-
-    CollisionResponse {
-        impulses: Vec::new(),
-        projection,
-    }
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
--- src/world.rs
+++ src/world.rs
@@ -7,9 +7,7 @@ use crate::context::{
     },
     GraphicsContext,
 };
-use crate::physics::{
-    calc_impulse, detect_collisions, Body, CollisionResponse,
-};
+use crate::physics::{detect_collisions, Body, CollisionConstraint};
 
 pub struct Object {
     pub body: Body,
@@ -66,7 +64,7 @@ impl World {
         let mut mass_adjustments = HashMap::new();
         for i in 0..self.objects.len() {
             for j in i + 1..self.objects.len() {
-                if let Some(mut manifold) = detect_collisions(
+                if let Some(manifold) = detect_collisions(
                     &self.objects[i].body,
                     &self.objects[j].body,
                 ) {
@@ -80,31 +78,40 @@ impl World {
             }
         }
 
-        let mut impulses = Vec::new();
+        let mut constraints = Vec::new();
         let mut projections = Vec::new();
-        for (i, j, collision) in collisions {
-            assert!(i < j);
-            let (head, tail) = self.objects.split_at_mut(j);
-            let CollisionResponse {
-                impulses: collision_impulses,
-                projection,
-            } = calc_impulse(
-                collision,
-                &mut head[i].body,
-                mass_adjustments[&i] as f32,
-                &mut tail[0].body,
-                mass_adjustments[&j] as f32,
-            );
-            for im in collision_impulses {
-                impulses.push((i, -im.impulse, im.position));
-                impulses.push((j, im.impulse, im.position));
-            }
+        for (i, j, manifold) in collisions {
+            // Do some linear projection to stop bodies from just sinking into
+            // each other
+            let slop_limit = 0.01f32;
+            let slop_amount = 0.8f32;
+            let projection = (manifold.depth - slop_limit).max(0.0)
+                * slop_amount
+                * manifold.normal;
+
             projections.push((i, -projection));
             projections.push((j, projection));
+            constraints.push((
+                i,
+                j,
+                CollisionConstraint::new(
+                    manifold,
+                    &self.objects[i].body,
+                    mass_adjustments[&i] as f32,
+                    &self.objects[j].body,
+                    mass_adjustments[&j] as f32,
+                ),
+            ));
         }
 
-        for (i, impulse, world_contact) in impulses {
-            self.objects[i].body.resolve_impulse(impulse, world_contact);
+        const SOLVER_ITERS: usize = 20;
+        for _ in 0..SOLVER_ITERS {
+            for (i, j, constraint) in constraints.iter_mut() {
+                assert!(i < j);
+
+                let (head, tail) = self.objects.split_at_mut(*j);
+                constraint.solve(&mut head[*i].body, &mut tail[0].body);
+            }
         }
 
         for (i, projection) in projections {
