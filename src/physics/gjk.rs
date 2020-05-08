102844037ceccbca7c930cead9ea87846b679213
Tyler Zhang <t-veor@users.noreply.github.com>
2020-05-09 00:30:29 +0100

    Add EPA

--- src/physics/gjk.rs
+++ src/physics/gjk.rs
@@ -1,7 +1,9 @@
-use super::MeshRef;
+use std::collections::HashMap;
+
+use super::{CollisionManifold, MeshRef};
 use crate::alg::triple_cross_product;
 use crate::mesh::Mesh;
-use crate::util::NotNaN;
+use crate::util::{NotNaN, EPSILON};
 use cgmath::{InnerSpace, Matrix2, SquareMatrix, Vector2, Vector4, Zero};
 
 fn support(m: MeshRef, direction: Vector4<f32>) -> Vector4<f32> {
@@ -184,13 +186,172 @@ fn gjk_intersection(
     let mut d = -a;
 
     loop {
-        a = support(p, d) - support(q, d);
+        a = support(p, d) - support(q, -d);
         if a.dot(d) < 0.0 {
             return None;
         }
         s.push(a);
         if s.nearest_simplex(&mut d) {
             return Some(s);
+        }
+    }
+}
+
+struct EPCell {
+    faces: [[usize; 3]; 4],
+    normal: Vector4<f32>,
+    distance: f32,
+}
+
+impl EPCell {
+    fn from_faces(
+        vertices: &Vec<Vector4<f32>>,
+        faces: [[usize; 3]; 4],
+    ) -> Self {
+        let mut unique_vertices = [0; 4];
+        let mut unique_vertices_count = 0;
+        'outer: for face in faces.iter() {
+            for vertex_idx in face.iter() {
+                if !&unique_vertices[0..unique_vertices_count]
+                    .contains(vertex_idx)
+                {
+                    unique_vertices[unique_vertices_count] = *vertex_idx;
+                    unique_vertices_count += 1;
+
+                    if unique_vertices_count >= 4 {
+                        break 'outer;
+                    }
+                }
+            }
+        }
+
+        let a = vertices[unique_vertices[0]];
+        let b = vertices[unique_vertices[1]];
+        let c = vertices[unique_vertices[2]];
+        let d = vertices[unique_vertices[3]];
+
+        let mut normal = triple_cross_product(b - a, c - a, d - a);
+        let mut distance = normal.dot(a);
+        if distance < 0.0 {
+            normal = -normal;
+            distance = -distance;
+        }
+
+        Self {
+            faces,
+            normal,
+            distance,
+        }
+    }
+}
+
+struct ExpandingPolytope {
+    vertices: Vec<Vector4<f32>>,
+    cells: Vec<EPCell>,
+}
+
+impl ExpandingPolytope {
+    fn from_simplex(simplex: Simplex) -> Self {
+        assert!(simplex.length == 5);
+        let vertices = simplex.vertices.to_vec();
+
+        let mut faces = Vec::new();
+        faces.reserve(10);
+        for i in 0..5 {
+            for j in (i + 1)..5 {
+                for k in (j + 1)..5 {
+                    faces.push([i, j, k]);
+                }
+            }
+        }
+
+        let mut cells = Vec::new();
+        cells.reserve(5);
+        for i in 0..5 {
+            let mut cell_faces = [[0, 0, 0]; 4];
+            let mut cell_face_count = 0;
+            for face in faces.iter() {
+                if !face.contains(&i) {
+                    cell_faces[cell_face_count] = *face;
+                    cell_face_count += 1;
+
+                    if cell_face_count >= 4 {
+                        break;
+                    }
+                }
+            }
+
+            cells.push(EPCell::from_faces(&vertices, cell_faces));
+        }
+
+        Self { vertices, cells }
+    }
+
+    fn expand(&mut self, extend_point: Vector4<f32>) {
+        let mut removed_faces = Vec::new();
+
+        let cells = &mut self.cells;
+        let vertices = &self.vertices;
+
+        cells.retain(|cell| {
+            let v0 = vertices[cell.faces[0][0]];
+            if cell.normal.dot(extend_point - v0) > 0.0 {
+                true
+            } else {
+                // Before returning, we need to add all the faces to the removed
+                // faces
+                for face in cell.faces.iter() {
+                    match removed_faces.iter().position(|f| f == face) {
+                        Some(i) => {
+                            // found a duplicate, remove it from the removed
+                            // faces
+                            removed_faces.remove(i);
+                        }
+                        None => removed_faces.push(*face),
+                    }
+                }
+                false
+            }
+        });
+
+        // push the new vertex
+        let vertex_idx = self.vertices.len();
+        self.vertices.push(extend_point);
+
+        // now add a new cell for each face
+        for face in removed_faces {
+            let mut cell_faces = [
+                [face[0], face[1], vertex_idx],
+                [face[0], face[2], vertex_idx],
+                [face[1], face[2], vertex_idx],
+                face,
+            ];
+            for f in cell_faces.iter_mut() {
+                f.sort_unstable();
+            }
+
+            self.cells
+                .push(EPCell::from_faces(&self.vertices, cell_faces));
+        }
+    }
+}
+
+fn epa(p: MeshRef, q: MeshRef, simplex: Simplex) -> Vector4<f32> {
+    let mut polytope = ExpandingPolytope::from_simplex(simplex);
+
+    loop {
+        let min_cell = polytope
+            .cells
+            .iter()
+            .min_by_key(|i| NotNaN::new(i.distance).unwrap())
+            .unwrap();
+        let normal = min_cell.normal;
+        let extend_point = support(p, normal) - support(q, -normal);
+
+        if extend_point.dot(normal) - min_cell.distance > EPSILON {
+            polytope.expand(extend_point);
+        } else {
+            return normal;
         }
     }
 }
